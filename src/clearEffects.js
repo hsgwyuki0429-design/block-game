f58338ede7449010767de3b582a5965ae4bd7460
hsgwyuki0429-design <hsgw.yuki0429@gmail.com>
2026-08-11 13:12:22 +0900

    clearEffects.js を更新

--- src/clearEffects.js
+++ src/clearEffects.js
@@ -15,7 +15,7 @@ const PALETTE = [
 ];
 
 const fxClamp = (v, a, b) => Math.max(a, Math.min(b, v));
-const easeOutCubic = (t) => 1 - Math.pow(1 - t, 3);
+const fxEaseOutCubic = (t) => 1 - Math.pow(1 - t, 3);
 const easeOutQuint = (t) => 1 - Math.pow(1 - t, 5);
 const easeInOut = (t) => t < 0.5 ? 2 * t * t : 1 - Math.pow(-2 * t + 2, 2) / 2;
 
